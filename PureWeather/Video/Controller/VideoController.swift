cc4902bcdc0186dbb0626c0301a8f328cb6488a1
沈正圆 <914138410@qq.com>
2018-12-24 14:37:22 +0800

    add

--- PureWeather/Video/Controller/VideoController.swift
+++ PureWeather/Video/Controller/VideoController.swift
@@ -10,21 +10,27 @@ import Foundation
 import AVFoundation
 class VideoController:BaseController{
     var audioPlayer:AVAudioPlayer?
+    @IBOutlet weak var coverIv: UIImageView!
     @IBAction func onCloseClick(_ sender: Any) {
         addLog(eventType: AnalyticsMgr.TYPE_CLICK, eventId: Event.CLOSE_CLICK, packageName: getPackageName())
         self.dismiss(animated: true, completion: nil)
     }
     override func viewDidLoad() {
         playMusic()
+        initView()
+    }
+    func initView(){
+        coverIv.layer.masksToBounds = true
+        coverIv.layer.cornerRadius = 100
     }
     override func viewWillDisappear(_ animated: Bool) {
         stopMusic()
     }
     func stopMusic(){
-        if (self.audioPlayer != nil)
-        {
+        if (self.audioPlayer != nil){
             self.audioPlayer?.stop()
         }
+        stopRotate()
     }
     func playMusic(){
         let fileUrl = Bundle.main.url(forResource: "hide",
@@ -36,6 +42,19 @@ class VideoController:BaseController{
         {
             self.audioPlayer?.play()
         }
-        
+        startRotate()
+    }
+    func startRotate() {
+        let rotationAnim = CABasicAnimation(keyPath: "transform.rotation.z")
+        rotationAnim.fromValue = 0
+        rotationAnim.toValue = Double.pi * 2
+        rotationAnim.repeatCount = MAXFLOAT
+        rotationAnim.duration = 10
+        rotationAnim.isRemovedOnCompletion = false
+        coverIv.layer.add(rotationAnim, forKey: nil)
+    }
+    func stopRotate() {
+        coverIv.layer.removeAllAnimations()
     }
 }
+
