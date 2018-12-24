5822203312ca112b92e8ba8f1f5b5a95c497e7bf
沈正圆 <914138410@qq.com>
2018-12-24 11:20:57 +0800

    add

--- PureWeather/Video/Controller/VideoController.swift
+++ PureWeather/Video/Controller/VideoController.swift
@@ -7,15 +7,35 @@
 //
 
 import Foundation
+import AVFoundation
 class VideoController:BaseController{
+    var audioPlayer:AVAudioPlayer?
     @IBAction func onCloseClick(_ sender: Any) {
         addLog(eventType: AnalyticsMgr.TYPE_CLICK, eventId: Event.CLOSE_CLICK, packageName: getPackageName())
         self.dismiss(animated: true, completion: nil)
     }
     override func viewDidLoad() {
-        VideoMgr.shared.getMusic()
+        playMusic()
     }
     override func viewWillDisappear(_ animated: Bool) {
+        stopMusic()
+    }
+    func stopMusic(){
+        if (self.audioPlayer != nil)
+        {
+            self.audioPlayer?.stop()
+        }
+    }
+    func playMusic(){
+        let fileUrl = Bundle.main.url(forResource: "hide",
+                                      withExtension: "mp3",
+                                      subdirectory: nil,
+                                      localization: nil)
+        self.audioPlayer = try! AVAudioPlayer.init(contentsOf: fileUrl!)
+        if (self.audioPlayer != nil)
+        {
+            self.audioPlayer?.play()
+        }
         
     }
 }
